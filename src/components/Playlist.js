8c6cbe83e802e831859621ddaab9de66e18e761a
Hugo Agbonon <hugo@agbonon.fr>
2017-05-30 22:05:41 +0200

    Convert React PropTypes to prop-types module
    (Tweaked output from React-PropTypes-to-prop-types react-codemod)

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { StyleSheet, View } from 'react-native'
 import Button from 'apsl-react-native-button'
 import { ContentsList } from './ContentsList'
