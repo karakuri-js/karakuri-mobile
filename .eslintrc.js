fa6287122bc22e90d1a45584c70b5961c27806db
Hugo Agbonon <hugo@agbonon.fr>
2017-06-06 23:31:26 +0200

    Add eslint rule to prevent more arrow functions in render

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,14 @@ module.exports = {
     rules: {
         'consistent-return': 0,
         'no-console': 0,
+        'react/jsx-no-bind': [
+          2,
+          {
+            allowArrowFunctions : false,
+            allowBind: false,
+            ignoreRefs: false,
+          }
+        ],
         semi: [2, 'never']
     }
 };
