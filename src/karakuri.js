65f7ace97b4a70685c597a175d907d34346d3665
Hugo Agbonon <hugo@agbonon.fr>
2017-06-09 00:10:21 +0200

    Implement all screens (except the drawer to change directories) in a navigator

--- src/karakuri.js
+++ src/karakuri.js
@@ -1,9 +1,21 @@
+import React from 'react'
 import { StackNavigator } from 'react-navigation'
+import { flattenNavigationParamsProps } from './lib/navigationUtils'
 
 import Connection from './components/Connection'
+import ContentsList from './components/ContentsList'
 import Home from './components/Home'
+import FilterList from './components/FilterList'
+import Playlist from './components/Playlist'
 
-export default StackNavigator({
+const AppNavigator = StackNavigator({
   Connection: { screen: Connection },
-  Home: { screen: Home },
+  Home: { screen: flattenNavigationParamsProps(Home) },
+  ContentsList: { screen: flattenNavigationParamsProps(ContentsList) },
+  FilterList: { screen: flattenNavigationParamsProps(FilterList) },
+  Playlist: { screen: flattenNavigationParamsProps(Playlist) },
 }, { headerMode: 'none' })
+
+// onNavigationStateChange={null} desactivates the internal logger
+// which makes a HUGE difference in performance.
+export default () => <AppNavigator onNavigationStateChange={null} />
