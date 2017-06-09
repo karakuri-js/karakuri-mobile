d83435dfd7e5d8884eaca976515365507dba4e11
Hugo Agbonon <hugo@agbonon.fr>
2017-06-10 01:11:24 +0200

    Move more logic out of the Home component and into actions
    Including contents ordering, addToPlaylist

--- src/reducers/karaoke.js
+++ src/reducers/karaoke.js
@@ -1,12 +1,51 @@
-import { CONTENTS_LOADED } from '../constants/actionTypes'
+import { uniq } from 'lodash'
+import { CONTENTS_LOADED, SELECT_DIRECTORY } from '../constants/actionTypes'
+import { getContentsPerDirectories, getContentsPerGroups } from '../lib/contentsFormatter'
 
-const initialState = { contents: [] }
+const initialState = { contents: [], directoryContents: [], groupContents: [] }
 
 export default function karaoke(state = initialState, action) {
   switch (action.type) {
-    case CONTENTS_LOADED:
+    case CONTENTS_LOADED: {
+      const { contents } = action
+      // Group contents per directory & groups, then create listview datasources
+      const contentsPerDirectories = getContentsPerDirectories(contents)
+      const contentsPerGroups = getContentsPerGroups(contents)
+      const groupsPerLettersAndDirectories = Object.keys(contentsPerDirectories).reduce(
+        (directoriesObj, dirName) => ({
+          ...directoriesObj,
+          [dirName]: contentsPerDirectories[dirName].map(content => content.group).reduce(
+            (alphabetListObj, groupName) => {
+              const letter = groupName[0].toUpperCase()
+              return {
+                ...alphabetListObj,
+                [letter]: uniq((alphabetListObj[letter] || []).concat(groupName)),
+              }
+            },
+            {},
+          ),
+        }),
+        {},
+      )
+
+      const directories = Object.keys(contentsPerDirectories)
+      const selectedDirectoryName = directories[0]
+
       return {
-        contents: action.contents,
+        allContents: contents,
+        directories: Object.keys(contentsPerDirectories),
+        contentsPerGroups,
+        groupsPerLettersAndDirectories,
+        selectedDirectoryName,
+        directoryGroups: groupsPerLettersAndDirectories[selectedDirectoryName],
+      }
+    }
+
+    case SELECT_DIRECTORY:
+      return {
+        ...state,
+        selectedDirectoryName: action.directoryName,
+        directoryGroups: state.groupsPerLettersAndDirectories[action.directoryName],
       }
 
     default:
