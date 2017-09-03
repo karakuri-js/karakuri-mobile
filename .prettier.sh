f8d97d26df604bdaaed4c4f6d816f6ddee245661
Hugo Agbonon <hugo@agbonon.fr>
2017-09-03 13:29:07 +0200

    Update prettier to v1.6.1

--- .prettier.sh
+++ .prettier.sh
@@ -2,10 +2,10 @@
 jsfiles=$(git diff --cached --name-only --diff-filter=ACM | grep '\.jsx\?$' | tr '\n' ' ')
 [ -z "$jsfiles" ] && exit 0
 
-diffs=$(node_modules/.bin/prettier --print-width 100 --no-semi --single-quote --trailing-comma all -l $jsfiles)
+diffs=$(node_modules/.bin/prettier -l $jsfiles)
 [ -z "$diffs" ] && exit 0
 
-command=$(node_modules/.bin/prettier --print-width 100 --no-semi --single-quote --trailing-comma all --write $diffs)
+command=$(node_modules/.bin/prettier --write $diffs)
 [ -z "$command" ]
 
 echo "JS files have been formatted with prettier, please add the changes before committing: "$diffs""
