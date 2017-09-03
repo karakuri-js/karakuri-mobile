6d283d37fe6132c35fd27c3f743b9b36c3f77191
Hugo Agbonon <hugo@agbonon.fr>
2017-09-03 18:30:18 +0200

    Create a favorites screen

---
+++ src/components/FavoritesScreen.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { addToPlaylist, toggleFavorite } from '../actions'
+
+import ContentsList from './ContentsList'
+
+export class FavoritesScreen extends Component {
+  static propTypes = {
+    addToPlaylist: PropTypes.func.isRequired,
+    allContents: PropTypes.arrayOf(PropTypes.object).isRequired,
+    favorites: PropTypes.object.isRequired,
+    toggleFavorite: PropTypes.func.isRequired,
+  }
+
+  isFavorite = content => this.props.favorites[content.id]
+
+  render() {
+    return (
+      <ContentsList
+        onSelect={this.props.addToPlaylist}
+        contents={this.props.allContents.filter(this.isFavorite)}
+        favorites={this.props.favorites}
+        title="Favorites"
+        toggleFavorite={this.props.toggleFavorite}
+      />
+    )
+  }
+}
+
+export default connect(
+  ({ contents: { allContents }, favorites }) => ({
+    allContents,
+    favorites,
+  }),
+  { addToPlaylist, toggleFavorite },
+)(FavoritesScreen)
