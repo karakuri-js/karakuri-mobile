d91b8c898a5bfcc5892db922e55b10e2f2acec6d
Hugo Agbonon <hugo@agbonon.fr>
2018-04-06 12:29:29 +0200

    refactoring - get selected content from navigation state

--- src/components/ContentsList.js
+++ src/components/ContentsList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { ListView, StyleSheet, Text, TextInput, View } from 'react-native'
 
--- src/reducers/contents.js
+++ src/reducers/contents.js
@@ -1,9 +1,12 @@
+import { NavigationActions } from 'react-navigation'
+
 import {
   CONTENTS_LOADED,
   SELECT_DIRECTORY,
   SELECT_GROUP,
-  SELECT_CONTENT,
 } from '../constants/actionTypes'
+
+import { CONTENT_SCREEN } from '../constants/screens'
 
 const initialState = {
   allContents: [],
@@ -32,10 +35,11 @@ export default function contents(state = initialState, action) {
         groupName: action.groupName,
       }
 
-    case SELECT_CONTENT:
+    case NavigationActions.NAVIGATE:
+      if (action.routeName !== CONTENT_SCREEN) return state
       return {
         ...state,
-        contentId: action.contentId,
+        contentId: action.params.contentId,
       }
 
     default:
