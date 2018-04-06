0fbf00f89d8caa7169d8b1af542743cf9671fc7a
Hugo Agbonon <hugo@agbonon.fr>
2018-04-06 12:46:45 +0200

    refactoring - Stop repeating the same code on evry screen, directly connect ContentsList

--- src/components/FavoritesScreen.js
+++ src/components/FavoritesScreen.js
@@ -1,32 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { addToPlaylist, toggleFavorite } from '../actions'
 import { getAugmentedFavoriteContents } from '../selectors/contents'
-import { provideGoToContentScreen } from './goToContentScreenHOC'
 
 import ContentsList from './ContentsList'
 
 const FavoritesScreen = props => (
-  <ContentsList
-    addToPlaylist={props.addToPlaylist}
-    contents={props.favoriteContents}
-    onSelect={props.goToContentScreen}
-    title="Favorites"
-    toggleFavorite={props.toggleFavorite}
-  />
+  <ContentsList contents={props.favoriteContents} title="Favorites" />
 )
 
 FavoritesScreen.propTypes = {
-  addToPlaylist: PropTypes.func.isRequired,
   favoriteContents: PropTypes.arrayOf(PropTypes.object).isRequired,
-  goToContentScreen: PropTypes.func.isRequired,
-  toggleFavorite: PropTypes.func.isRequired,
 }
 
-export default connect(
-  state => ({
-    favoriteContents: getAugmentedFavoriteContents(state),
-  }),
-  { addToPlaylist, toggleFavorite },
-)(provideGoToContentScreen(FavoritesScreen))
+export default connect(state => ({
+  favoriteContents: getAugmentedFavoriteContents(state),
+}))(FavoritesScreen)
--- src/components/HistoryScreen.js
+++ src/components/HistoryScreen.js
@@ -2,29 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import { addToPlaylist, toggleFavorite } from '../actions'
 import { getAugmentedHistoryContents } from '../selectors/contents'
-import { provideGoToContentScreen } from './goToContentScreenHOC'
 
 import ContentsList from './ContentsList'
 
-export const HistoryScreen = ({ historyContents, ...props }) => (
-  <ContentsList
-    addToPlaylist={props.addToPlaylist}
-    contents={historyContents}
-    onSelect={props.goToContentScreen}
-    toggleFavorite={props.toggleFavorite}
-  />
-)
+export const HistoryScreen = ({ historyContents }) => <ContentsList contents={historyContents} />
 
 HistoryScreen.propTypes = {
-  addToPlaylist: PropTypes.func.isRequired,
   historyContents: PropTypes.arrayOf(PropTypes.object).isRequired,
-  goToContentScreen: PropTypes.func.isRequired,
-  toggleFavorite: PropTypes.func.isRequired,
 }
 
-export default connect(state => ({ historyContents: getAugmentedHistoryContents(state) }), {
-  addToPlaylist,
-  toggleFavorite,
-})(provideGoToContentScreen(HistoryScreen))
+export default connect(state => ({ historyContents: getAugmentedHistoryContents(state) }))(
+  HistoryScreen,
+)
